fabedd042c557d251f18f391623b1c19dead3b9b
sankalp-wono <sankalp.wono@gmail.com>
2025-07-29 18:38:12 +0530

    created schemas and configurations

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,7 @@ import errorHandler from "./middleware/errorHandler.js";
 import { config } from "dotenv";
 import connectDb from "./config/db.js";
 import mongoose from "mongoose";
-import userRoute from "./routes/userRoutes.js";
-import partnershipRoute from "./routes/partnershipRoute.js";
-import consultationRoute from "./routes/consultationRoute.js";
-import contactRoute from "./routes/contactRoute.js";
+
 
 const app = express();
 config();
@@ -19,10 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/auth", userRoute);
-app.use("/api/partnership", partnershipRoute);
-app.use("/api/consultation", consultationRoute);
-app.use("/api/contact", contactRoute);
+
 app.use(errorHandler);
 app.listen(
   PORT,
