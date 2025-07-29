01b53c8fb7ed94594d5c05a6a59723275c8b6bc4
AiwinrajWono <aiwinraj.wono@gmail.com>
2025-07-29 16:37:13 +0530

    `Update frontend code to use Poppins font and Tailwind CSS configuration`

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,22 +21,16 @@ module.exports = {
         accent: "#eaa415",
       },
       fontFamily: {
-        sans: ["Elmessiri", "ui-sans-serif", "system-ui"],
+        sans: ["Poppins", "ui-sans-serif", "system-ui"],
         heading: ["Poppins", "ui-sans-serif", "system-ui"],
         mono: ["Fira Code", "ui-monospace"],
       },
       fontSize: {
         tiny: ["0.75rem", { lineHeight: "1rem" }], // ~12px
-        small: ["0.875rem", { lineHeight: "1.25rem" }], // ~14px
-        base: ["1rem", { lineHeight: "1.5rem" }], // 16px
-        body: ["1.125rem", { lineHeight: "1.75rem" }], // ~18px
+        content: ["1rem", { lineHeight: "1.5rem" }], // 16px
         subtitle: ["1.25rem", { lineHeight: "1.75rem" }], // ~20px
         title: ["1.5rem", { lineHeight: "2rem" }], // ~24px
-        headline: ["1.875rem", { lineHeight: "2.25rem" }], // ~30px
-        display: ["2.25rem", { lineHeight: "2.5rem" }], // ~36px
         hero: ["3rem", { lineHeight: "1" }], // 48px
-        mega: ["3.75rem", { lineHeight: "1" }], // 60px
-        full: ["100%", { lineHeight: "1" }], // 60px
       },
       // etc...
     },
