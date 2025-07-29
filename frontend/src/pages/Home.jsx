9a30a57ceaa17f01b645be782530fead97e664ef
AiwinrajWono <aiwinraj.wono@gmail.com>
2025-07-29 16:37:20 +0530

    `Refactored Home page layout and added new components`

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,34 +1,51 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import PrimaryButton from "../components/PrimaryButton";
 
 const Home = () => {
   return (
     <div>
-      <section className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <div className="flex flex-col gap-4 justify-end items-start">
-          <p className="uppercase font-semibold text-title lg:text-[6rem] lg:leading-normal">
-            WORLDS NOMAD COMMUNITY
-          </p>
-          <span className="text-[2rem]">
-            Connecting Co-working Spaces and Flexible Workers
-          </span>
-        </div>
-        <div className=" rounded-xl overflow-hidden">
-          <div className="bg-[url('/images/bg-image.jpg')] bg-cover bg-center h-full w-full rounded-md shadow-md flex items-end">
-            <div className="bg-white/10 backdrop-blur-md p-4 w-full flex flex-col gap-4">
-              <span className="text-white">
-                Serves as a dynamic platform, seamlessly connecting freelance
-                professionals, remote workers, and individuals seeking flexible
-                workspace
-              </span>
-              <hr />
-              <div className="flex items-center gap-2">
-              <div className="p-3 rounded-2xl bg-white">
-                <span>Ratings here</span>
-              </div>
-              <NavLink className={"text-white text-sm hover:underline"}>View More Reviews</NavLink>
+      <section className="flex flex-col gap-2">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <div className="flex flex-col gap-4 justify-end items-start">
+            <p className="uppercase font-semibold text-title lg:text-[6rem] lg:leading-normal">
+              WORLDS NOMAD COMMUNITY
+            </p>
+            <span className="text-[2rem]">
+              Connecting Co-working Spaces and Flexible Workers
+            </span>
+          </div>
+          <div className=" rounded-xl overflow-hidden">
+            <div className="bg-[url('/images/bg-image.jpg')] bg-cover bg-center h-full w-full rounded-md shadow-md flex items-end">
+              <div className="bg-white/10 backdrop-blur-md p-4 w-full flex flex-col gap-4">
+                <span className="text-white">
+                  Serves as a dynamic platform, seamlessly connecting freelance
+                  professionals, remote workers, and individuals seeking
+                  flexible workspace
+                </span>
+                <hr />
+                <div className="flex items-center gap-2">
+                  <div className="p-3 rounded-2xl bg-white">
+                    <span>Ratings here</span>
+                  </div>
+                  <NavLink className={"text-white text-sm hover:underline"}>
+                    View More Reviews
+                  </NavLink>
+                </div>
               </div>
             </div>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols02 gap-2">
+            <div className="flex flex-col gap-2">
+                <div className="bg-white text-black">
+                  Avatars here
+                </div>
+                <PrimaryButton externalStyles={"bg-black"} />
+            </div>
+            <div className="flex flex-col gap-2">
+
+            </div>
+        
           </div>
         </div>
       </section>
