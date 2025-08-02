856997d9930bd3aa02fc906cfdebac11ba2e336c
sankalp-wono <sankalp.wono@gmail.com>
2025-08-02 17:05:52 +0530

    test

--- backend/controllers/commonCompanyControllers.js
+++ backend/controllers/commonCompanyControllers.js
@@ -3,6 +3,11 @@ import CoworkingInclusions from "../models/coworking/Inclusions.js";
 import CoworkingReviews from "../models/coworking/Review.js";
 import CoworkingServices from "../models/coworking/Services.js";
 import CoworkingPointOfContact from "../models/coworking/PointOfContact.js";
+import ColivingCompany from "../models/coliving/ColivingCompany.js";
+import ColivingInclusions from "../models/coliving/ColivingInclusions.js";
+import ColivingReviews from "../models/coliving/Reviews.js";
+import ColivingPointOfContact from "../models/coliving/PointOfContact.js";
+import ColivingUnits from "../models/coliving/Units.js";
 
 export const getCompanyDataLocationWise = async (req, res, next) => {
   try {
@@ -60,7 +65,57 @@ export const getCompanyDataLocationWise = async (req, res, next) => {
       });
 
       return res.status(200).json(enrichedCompanies);
+    } else if (category?.toLowerCase() === "coliving") {
+      const [
+        colivingCompanies,
+        colivingInclusions,
+        colivingPoc,
+        colivingUnits,
+        colivingReviews,
+      ] = await Promise.all([
+        ColivingCompany.find({
+          country: { $regex: `^${country}$`, $options: "i" },
+          state: { $regex: `^${state}$`, $options: "i" },
+        })
+          .lean()
+          .exec(),
+        ColivingInclusions.find().lean().exec(),
+        ColivingPointOfContact.find({ isActive: true }).lean().exec(),
+        ColivingUnits.find().lean().exec(),
+        ColivingReviews.find().lean().exec(),
+      ]);
+
+      const enrichedCompanies = colivingCompanies.map((company) => {
+        const companyId = company._id.toString();
+
+        const companyInclusions = colivingInclusions.filter(
+          (item) => item.colivingCompany?.toString() === companyId
+        );
+
+        const companyPOCs = colivingPoc.filter(
+          (item) => item.colivingCompany?.toString() === companyId
+        );
+
+        const companyUnits = colivingUnits.filter(
+          (item) => item.colivingCompany?.toString() === companyId
+        );
+
+        const companyReviews = colivingReviews.filter(
+          (item) => item.colivingCompany?.toString() === companyId
+        );
+
+        return {
+          ...company,
+          inclusions: companyInclusions,
+          pointOfContacts: companyPOCs,
+          units: companyUnits,
+          reviews: companyReviews,
+        };
+      });
+
+      return res.status(200).json(enrichedCompanies);
     }
+    return res.status(404).json({ message: "No resource found" });
   } catch (error) {
     next(error);
   }
