fcce8f898d0b8b39d7e9b966c419bb5f99e0f894
allanwono <allan.wono@gmail.com>
2025-08-23 17:45:05 +0530

    text change in news & blog

--- frontend/src/components/BlogFetch.jsx
+++ frontend/src/components/BlogFetch.jsx
@@ -83,7 +83,7 @@ const BlogFetch = () => {
   return (
     <div className="my-6">
       <div className="flex justify-between items-center mb-4 flex-col sm:flex-col xs:flex-col md:flex-row lg:flex-row">
-        <h2 className="text-title font-semibold text-host">Local Blog</h2>
+        <h2 className="text-title font-semibold text-host">Blog</h2>
         {/* Controls */}
         <div className="flex items-center justify-end gap-3 mb-5">
           <label className="text-sm font-medium text-gray-700">
