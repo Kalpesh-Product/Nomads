865b1b20ff45073b47e7100473b972e0c1e447ad
muskan-wono <muskan.wono@gmail.com>
2025-09-09 10:59:39 +0530

    poc controllers

--- backend/routes/pocRoutes.js
+++ backend/routes/pocRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import upload from "../config/multerConfig.js";
-import { bulkInsertPoc } from "../controllers/pocControllers.js";
+import { bulkInsertPoc, getPocDetails } from "../controllers/pocControllers.js";
 
 const router = Router();
 router.post("/bulk-insert-poc", upload.single("poc"), bulkInsertPoc);
+router.get("/poc", getPocDetails);
 export default router;
