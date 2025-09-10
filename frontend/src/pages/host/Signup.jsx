063326fb49df6dc43fddd34820e10ba9a13d763f
allanwono <allan.wono@gmail.com>
2025-09-10 15:40:36 +0530

    form validation

--- frontend/src/pages/host/Signup.jsx
+++ frontend/src/pages/host/Signup.jsx
@@ -12,7 +12,7 @@ import {
 } from "@mui/material";
 import Container from "../../components/Container";
 import GetStartedButton from "../../components/GetStartedButton";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import axios from "../../utils/axios";
 import toast from "react-hot-toast";
@@ -27,7 +27,7 @@ const steps = [
   "Company Info",
   "Create Website Info",
   "Services",
-  "Review",
+  "Activation",
 ];
 
 const serviceOptions = [
@@ -64,6 +64,8 @@ const serviceOptions = [
 ];
 
 const HostSignup = () => {
+  const navigate = useNavigate();
+
   const [activeStep, setActiveStep] = useState(0);
 
   const { control, handleSubmit, getValues, trigger, reset, watch, setValue } =
@@ -139,24 +141,39 @@ const HostSignup = () => {
   //     reset();
   //   },
   // });
+  // const { mutate: register, isLoading: isRegisterLoading } = useMutation({
+  //   mutationFn: async (fd) => {
+  //     const response = await axios.post("forms/register-form-submission", fd, {
+  //       headers: { "Content-Type": "multipart/form-data" },
+  //     });
+  //     return response.data;
+  //   },
+  //   onSuccess: (data) => {
+  //     toast.success("Form submitted successfully");
+  //     reset();
+  //   },
+  //   onError: (error) => {
+  //     toast.error(error.response?.data?.message || "Something went wrong");
+  //     reset();
+  //   },
+  // });
+
   const { mutate: register, isLoading: isRegisterLoading } = useMutation({
     mutationFn: async (fd) => {
-      const response = await axios.post(
-        "forms/register-form-submission",
-        fd,
-        {
-          headers: { "Content-Type": "multipart/form-data" },
-        }
-      );
+      const response = await axios.post("forms/register-form-submission", fd, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
       return response.data;
     },
     onSuccess: (data) => {
       toast.success("Form submitted successfully");
       reset();
+      setActiveStep((prev) => prev + 1); // 👈 go to Step 5 after submit
     },
     onError: (error) => {
       toast.error(error.response?.data?.message || "Something went wrong");
       reset();
+      setActiveStep((prev) => prev + 1); // 👈 go to Step 5 after submit (To be removed later)
     },
   });
 
@@ -266,7 +283,7 @@ const HostSignup = () => {
             <Controller
               name="country"
               control={control}
-              rules={{ required: "Country is required" }}
+              // rules={{ required: "Country is required" }}
               render={({ field, fieldState }) => (
                 <TextField
                   {...field}
@@ -295,7 +312,7 @@ const HostSignup = () => {
             <Controller
               name="state"
               control={control}
-              rules={{ required: "State is required" }}
+              // rules={{ required: "State is required" }}
               render={({ field, fieldState }) => {
                 const countryName = watch("country");
                 const countryObj = Country.getAllCountries().find(
@@ -334,7 +351,7 @@ const HostSignup = () => {
             <Controller
               name="city"
               control={control}
-              rules={{ required: "City is required" }}
+              // rules={{ required: "City is required" }}
               render={({ field, fieldState }) => {
                 const countryName = watch("country");
                 const stateName = watch("state");
@@ -385,7 +402,7 @@ const HostSignup = () => {
             <Controller
               name="companyName"
               control={control}
-              rules={{ required: "Company Name is required" }}
+              // rules={{ required: "Company Name is required" }}
               render={({ field, fieldState }) => (
                 <TextField
                   {...field}
@@ -401,7 +418,7 @@ const HostSignup = () => {
             <Controller
               name="industry"
               control={control}
-              rules={{ required: "Industry is required" }}
+              // rules={{ required: "Industry is required" }}
               render={({ field, fieldState }) => (
                 <TextField
                   {...field}
@@ -418,7 +435,7 @@ const HostSignup = () => {
               name="companySize"
               control={control}
               rules={{
-                required: "Company Size is required",
+                // required: "Company Size is required",
                 pattern: {
                   value: /^[0-9]+$/, // only digits allowed
                   message: "Company Size must be a number",
@@ -439,7 +456,7 @@ const HostSignup = () => {
             <Controller
               name="companyCountry"
               control={control}
-              rules={{ required: "Company Country is required" }}
+              // rules={{ required: "Company Country is required" }}
               render={({ field, fieldState }) => (
                 <TextField
                   {...field}
@@ -468,7 +485,7 @@ const HostSignup = () => {
             <Controller
               name="companyState"
               control={control}
-              rules={{ required: "Company State is required" }}
+              // rules={{ required: "Company State is required" }}
               render={({ field, fieldState }) => {
                 const countryName = watch("companyCountry");
                 const countryObj = Country.getAllCountries().find(
@@ -507,7 +524,7 @@ const HostSignup = () => {
             <Controller
               name="companyCity"
               control={control}
-              rules={{ required: "Company City is required" }}
+              // rules={{ required: "Company City is required" }}
               render={({ field, fieldState }) => {
                 const countryName = watch("companyCountry");
                 const stateName = watch("companyState");
@@ -562,7 +579,7 @@ const HostSignup = () => {
               <Controller
                 name="title"
                 control={control}
-                rules={{ required: "Website Title is required" }}
+                // rules={{ required: "Website Title is required" }}
                 render={({ field, fieldState }) => (
                   <TextField
                     {...field}
@@ -578,7 +595,7 @@ const HostSignup = () => {
               <Controller
                 name="subTitle"
                 control={control}
-                rules={{ required: "Website Subtitle is required" }}
+                // rules={{ required: "Website Subtitle is required" }}
                 render={({ field, fieldState }) => (
                   <TextField
                     {...field}
@@ -667,7 +684,7 @@ const HostSignup = () => {
                   <Controller
                     name={`about.${index}.text`}
                     control={control}
-                    rules={{ required: "About paragraph is required" }}
+                    // rules={{ required: "About paragraph is required" }}
                     render={({ field, fieldState }) => (
                       <TextField
                         {...field}
@@ -733,7 +750,7 @@ const HostSignup = () => {
                     <Controller
                       name={`products.${index}.name`}
                       control={control}
-                      rules={{ required: "Name is required" }}
+                      // rules={{ required: "Name is required" }}
                       render={({ field, fieldState }) => (
                         <TextField
                           {...field}
@@ -750,7 +767,7 @@ const HostSignup = () => {
                     <Controller
                       name={`products.${index}.type`}
                       control={control}
-                      rules={{ required: "Type is required" }}
+                      // rules={{ required: "Type is required" }}
                       render={({ field, fieldState }) => (
                         <TextField
                           {...field}
@@ -780,7 +797,7 @@ const HostSignup = () => {
                     <Controller
                       name={`products.${index}.description`}
                       control={control}
-                      rules={{ required: "Description is required" }}
+                      // rules={{ required: "Description is required" }}
                       render={({ field, fieldState }) => (
                         <TextField
                           {...field}
@@ -799,7 +816,7 @@ const HostSignup = () => {
                     <Controller
                       name={`products.${index}.cost`}
                       control={control}
-                      rules={{ required: "Cost is required" }}
+                      // rules={{ required: "Cost is required" }}
                       render={({ field, fieldState }) => (
                         <TextField
                           {...field}
@@ -883,7 +900,7 @@ const HostSignup = () => {
                 name="websiteEmail"
                 control={control}
                 rules={{
-                  required: "Contact Email is required",
+                  // required: "Contact Email is required",
                   pattern: {
                     value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
                     message: "Invalid email format",
@@ -905,7 +922,7 @@ const HostSignup = () => {
               <Controller
                 name="phone"
                 control={control}
-                rules={{ required: "Contact Phone is required" }}
+                // rules={{ required: "Contact Phone is required" }}
                 render={({ field, fieldState }) => (
                   <TextField
                     {...field}
@@ -922,20 +939,20 @@ const HostSignup = () => {
               <Controller
                 name="mapUrl"
                 control={control}
-                rules={{
-                  required: "Map URL is required",
-                  validate: (val) => {
-                    const MAP_EMBED_REGEX =
-                      /^https?:\/\/(www\.)?(google\.com|maps\.google\.com)\/maps\/embed(\/v1\/[a-z]+|\?pb=|\/?\?)/i;
+                // rules={{
+                //   required: "Map URL is required",
+                //   validate: (val) => {
+                //     const MAP_EMBED_REGEX =
+                //       /^https?:\/\/(www\.)?(google\.com|maps\.google\.com)\/maps\/embed(\/v1\/[a-z]+|\?pb=|\/?\?)/i;
 
-                    const v = (val || "").trim();
+                //     const v = (val || "").trim();
 
-                    return (
-                      MAP_EMBED_REGEX.test(v) ||
-                      "Enter a valid Google Maps *embed* URL (e.g. https://www.google.com/maps/embed?pb=...)"
-                    );
-                  },
-                }}
+                //     return (
+                //       MAP_EMBED_REGEX.test(v) ||
+                //       "Enter a valid Google Maps *embed* URL (e.g. https://www.google.com/maps/embed?pb=...)"
+                //     );
+                //   },
+                // }}
                 render={({ field, fieldState }) => (
                   <TextField
                     {...field}
@@ -962,7 +979,7 @@ const HostSignup = () => {
               <Controller
                 name="address"
                 control={control}
-                rules={{ required: "Address is required" }}
+                // rules={{ required: "Address is required" }}
                 render={({ field, fieldState }) => (
                   <TextField
                     {...field}
@@ -981,7 +998,7 @@ const HostSignup = () => {
               <Controller
                 name="registeredCompanyName"
                 control={control}
-                rules={{ required: "Registered Company Name is required" }}
+                // rules={{ required: "Registered Company Name is required" }}
                 render={({ field, fieldState }) => (
                   <TextField
                     {...field}
@@ -998,7 +1015,7 @@ const HostSignup = () => {
               <Controller
                 name="copyrightText"
                 control={control}
-                rules={{ required: "Copyright Text is required" }}
+                // rules={{ required: "Copyright Text is required" }}
                 render={({ field, fieldState }) => (
                   <TextField
                     {...field}
@@ -1027,10 +1044,10 @@ const HostSignup = () => {
               name="selectedServices"
               control={control}
               defaultValue={["Website Builder", "Lead Generation"]}
-              rules={{
-                validate: (value) =>
-                  value.length > 0 || "Please select at least one service",
-              }}
+              // rules={{
+              //   validate: (value) =>
+              //     value.length > 0 || "Please select at least one service",
+              // }}
               render={({ field, fieldState }) => {
                 const mandatoryServices = [
                   "Website Builder",
@@ -1150,7 +1167,8 @@ const HostSignup = () => {
                 </p>
                 <p>
                   Our team will reach out to you shortly for more details and
-                  will inform you once your website is activated.
+                  will inform you once your website is activated within 48
+                  hours.
                 </p>
                 <br />
                 <p>
@@ -1299,28 +1317,65 @@ const HostSignup = () => {
           })}>
           {renderStepFields()}
           <div className="col-span-1 lg:col-span-2 flex justify-between items-center">
-            {activeStep > 0 && (
+            {/* {activeStep > 0 && (
+              <GetStartedButton
+                title="Back"
+                handleSubmit={() => setActiveStep((prev) => prev - 1)}
+              />
+            )} */}
+            {activeStep > 0 && activeStep < stepFields.length - 1 && (
               <GetStartedButton
                 title="Back"
                 handleSubmit={() => setActiveStep((prev) => prev - 1)}
               />
             )}
+
             {activeStep === 0 && (
               <div className="flex  justify-center  items-center w-full">
                 <GetStartedButton title="Next" handleSubmit={handleNext} />
               </div>
             )}
 
-            {activeStep !== 0 && activeStep < steps.length - 1 ? (
+            {activeStep !== 0 && activeStep < steps.length - 2 ? (
               <div className="flex  justify-center lg:justify-end items-center w-full">
                 <GetStartedButton title="Next" handleSubmit={handleNext} />
               </div>
             ) : (
               <></>
             )}
-            {activeStep === stepFields.length - 1 && (
+            {/* {activeStep === stepFields.length - 2 && (
               <div className="flex  justify-center lg:justify-end  items-center w-full">
                 <GetStartedButton title="Submit" type={"submit"} />
+              </div>
+            )}
+            {activeStep === stepFields.length - 1 && (
+              <div className="flex justify-center lg:justify-end items-center w-full">
+                <GetStartedButton
+                  title="Go To Home"
+                  handleSubmit={() => {
+                    // 👉 replace with your actual navigation logic
+                    window.location.href = "/";
+                  }}
+                />
+              </div>
+            )} */}
+
+            {/* Submit button on 4th step */}
+            {activeStep === stepFields.length - 2 && (
+              <div className="flex justify-center lg:justify-end items-center w-full">
+                <GetStartedButton title="Submit" type="submit" />
+              </div>
+            )}
+
+            {/* Go To Home button on 5th step */}
+            {activeStep === stepFields.length - 1 && (
+              <div className="flex justify-center items-center w-full">
+                <GetStartedButton
+                  title="Go To Home"
+                  handleSubmit={() => {
+                    navigate("/"); // 👈 React Router navigation
+                  }}
+                />
               </div>
             )}
           </div>
