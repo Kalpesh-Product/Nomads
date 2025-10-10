3a9c5e973dc1eb49a2710515adf4f76107d074b7
muskan-wono <muskan.wono@gmail.com>
2025-10-10 18:43:25 +0530

    typo

--- backend/models/NomadUser.js
+++ backend/models/NomadUser.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
       required: [true, "Password is required"],
       minlength: [6, "Password must be at least 6 characters long"],
     },
-    country: {s
+    country: {
       type: String,
       trim: true,
     },
