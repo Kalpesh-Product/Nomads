889144a9d67f6e240b4898fd70dc35d27fb1c3e1
allanwono <allan.wono@gmail.com>
2026-01-14 11:07:28 +0530

    bg black for image modal in blog details

--- frontend/src/pages/BlogDetails.jsx
+++ frontend/src/pages/BlogDetails.jsx
@@ -127,7 +127,7 @@ const BlogDetails = () => {
 
       {activeImage && (
         <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black/70 p-4"
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black p-4"
           onClick={handleImageClose}
         >
           <div
