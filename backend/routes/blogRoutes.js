5dbd7002d453f8b8cc97b14acd51baeb0313ff02
allanwono <allan.wono@gmail.com>
2026-02-20 17:43:41 +0530

    added get-news & get-blogs controllers

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import {
-    getBlogs,
-    bulkInsertBlogs,
-    createBlog,
-    updateBlog,
-    deleteBlog,
+  getBlogs,
+  bulkInsertBlogs,
+  createBlog,
+  updateBlog,
+  deleteBlog,
 } from "../controllers/blogController.js";
 import upload from "../config/multerConfig.js";
 
 const router = Router();
 
 router.get("/blogs", getBlogs);
+router.get("/get-blogs", getBlogs);
 router.post("/blogs", createBlog);
 router.put("/blogs/:id", updateBlog);
 router.delete("/blogs/:id", deleteBlog);
--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -1,16 +1,17 @@
 // routes/newsRoutes.js
 import { Router } from "express";
 import {
-    getNews,
-    bulkInsertnews,
-    createNews,
-    updateNews,
-    deleteNews,
+  getNews,
+  bulkInsertnews,
+  createNews,
+  updateNews,
+  deleteNews,
 } from "../controllers/newsController.js";
 import upload from "../config/multerConfig.js";
 
 const router = Router();
 router.get("/news", getNews);
+router.get("/get-news", getNews);
 router.post("/news", createNews);
 router.put("/news/:id", updateNews);
 router.delete("/news/:id", deleteNews);
