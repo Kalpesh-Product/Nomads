19753b10bc8156ce6e23d5e7fd224b5daf8baabd
allanwono <allan.wono@gmail.com>
2026-03-16 12:00:15 +0530

    1st cut compatible search

---
+++ frontend/src/pages/AiCompatibleSearch.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AiCompatibleSearch = () => {
+  return <div>AiCompatibleSearch</div>;
+};
+
+export default AiCompatibleSearch;
---
+++ frontend/src/pages/AiCompatibleSearchResults.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AiCompatibleSearchResults = () => {
+  return <div>AiCompatibleSearchResults</div>;
+};
+
+export default AiCompatibleSearchResults;
