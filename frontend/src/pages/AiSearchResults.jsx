33b307b342a241a362aa155ceefcccb5c350ee9d
allanwono <allan.wono@gmail.com>
2026-03-27 17:16:00 +0530

    padding alignment in phone view

--- frontend/src/pages/AiSearchResults.jsx
+++ frontend/src/pages/AiSearchResults.jsx
@@ -418,7 +418,7 @@ const AiSearchResults = () => {
   return (
     <div className="min-h-full bg-white">
       <main className="pb-8">
-        <div className="mx-0 w-full max-w-[80rem] px-1 sm:px-6 lg:mx-auto lg:max-w-[80rem] lg:px-0 lg:min-w-[75%]">
+        <div className="mx-0 w-full max-w-[80rem] px-3 sm:px-6 lg:mx-auto lg:max-w-[80rem] lg:px-0 lg:min-w-[75%]">
           <div className="rounded-[10px] bg-white px-0 pb-6">
             <div className="flex items-center gap-2">
               <button
