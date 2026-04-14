cdc29a119427a3f1b8928b5d59c78b105a6336e8
pranav-wono <pranavv.wono@gmail.com>
2026-04-14 11:12:35 +0530

    feat: add controller to retrieve state-wise weights with calculated scores

--- backend/controllers/stateWiseWeightController.js
+++ backend/controllers/stateWiseWeightController.js
@@ -3,23 +3,19 @@ import { stateWiseWeightCalculation } from "../controllers/stateWiseWeightCalcul
 
 export const getStateWiseWeight = async (req, res, next) => {
     try {
-        // 1. Fetch data from Database
         const stateWiseWeight = await StateWiseWeight.find().populate({
             path: "company",
             select: "continent, country, state"
         });
 
-        // 2. Loop through each city and calculate the 37 formulas
         const dataWithScores = stateWiseWeight.map(item => {
             const plainItem = item.toObject();
 
-            // 3. Directly pass the DB weights to the calculation function!
             plainItem.calculatedScores = stateWiseWeightCalculation(plainItem.weight);
 
             return plainItem;
         });
 
-        // 4. Send the final response
         res.status(200).json({
             success: true,
             count: dataWithScores.length,
