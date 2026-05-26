4a97efc9855dad08ea76c83fda345a54258ca184
pranav-wono <pranavv.wono@gmail.com>
2026-05-26 15:41:04 +0530

    o in blue color

--- frontend/src/pages/AiHome.jsx
+++ frontend/src/pages/AiHome.jsx
@@ -115,8 +115,8 @@ const AiHome = () => {
     text.split("").map((char, index) => {
       const normalizedText = text.toLowerCase();
       const inOfWord =
-        normalizedText.slice(index, index + 2) === "of" ||
-        normalizedText.slice(index - 1, index + 1) === "of";
+        normalizedText.slice(index, index + 2) === "" ||
+        normalizedText.slice(index - 1, index + 1) === "";
       const isBlueO = (char === "o" || char === "O") && !inOfWord;
       return (
         <span
