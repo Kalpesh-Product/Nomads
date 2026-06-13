f91cd35617a4335c56772ef5d17326a19978efcb
allanwono <allan.wono@gmail.com>
2026-06-13 15:19:51 +0530

    searchbar & font size

--- frontend/src/pages/AiGlobalListingsList.jsx
+++ frontend/src/pages/AiGlobalListingsList.jsx
@@ -1111,7 +1111,7 @@ const AiGlobalListingsList = () => {
             onBack={() => navigate(-1)}
             onClear={() => navigate(-1)}
             heading={
-              <p className=" mt-6 mb-6 flex items-center gap-2 text-sm font-medium leading-snug text-black/85 lg:text-[0.8rem] font-play">
+              <p className=" mt-6 mb-6 flex items-center gap-2 text-sm font-medium leading-snug text-black/85 lg:text-[0.9rem] font-play">
                 {!isSecondHeadingPhase && (
                   <span
                     className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-black border-b-transparent"
