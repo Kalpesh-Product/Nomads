3133a830d94d490a80a154a0cd3c1aabc98cfb99
allanwono <allan.wono@gmail.com>
2026-06-20 16:09:12 +0530

    APIs for Add Edit & Update Ststus Event

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,15 +1,21 @@
 import { Router } from "express";
 import upload from "../config/multerConfig.js";
 import {
+  addEvent,
   bulkInsertEvents,
   getEventsByDestination,
   getEvents,
+  updateEvent,
+  updateEventStatus,
 } from "../controllers/eventController.js";
 
 const router = Router();
 
 router.get("/", getEvents);
 router.get("/destination/:destination", getEventsByDestination);
+router.post("/", addEvent);
 router.post("/bulk-insert", upload.single("events-file"), bulkInsertEvents);
+router.patch("/:eventId/status", updateEventStatus);
+router.patch("/:eventId", updateEvent);
 
 export default router;
