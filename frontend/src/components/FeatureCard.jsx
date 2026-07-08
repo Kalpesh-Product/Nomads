4c1fdc7a4218837c031652d23f62815aaa3b0188
allanwono <allan.wono@gmail.com>
2026-07-08 11:49:31 +0530

    separator thick and thin lines in key modules for your business

--- frontend/src/components/FeatureCard.jsx
+++ frontend/src/components/FeatureCard.jsx
@@ -3,7 +3,7 @@ const FeatureCard = ({ title, icon }) => {
     <div
       className="w-full rounded-xl"
     >
-      <div className="flex justify-center items-center gap-4 py-4 flex-col border-b-4 border-y-gray-200 transition-all hover:border-black">
+      <div className="flex justify-center items-center gap-4 py-4 flex-col border-b-2 border-y-gray-200 transition-all hover:border-black">
         <div
           style={{
             width: "2.5rem",
