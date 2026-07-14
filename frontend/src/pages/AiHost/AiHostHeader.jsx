c0278211835caa85958a536c034e30c4c03323e2
allanwono <allan.wono@gmail.com>
2026-07-14 12:01:31 +0530

    Become a nomad link changes

--- frontend/src/pages/AiHost/AiHostHeader.jsx
+++ frontend/src/pages/AiHost/AiHostHeader.jsx
@@ -111,9 +111,9 @@ const AiHostHeader = ({ onMobileSidebarToggle }) => {
 
   const goToHosts = () => {
     if (window.location.hostname.includes("localhost")) {
-      window.location.href = "http://nomad.localhost:5173/home";
+      window.location.href = "http://nomad.localhost:5173";
     } else {
-      window.location.href = "https://nomad.wono.co/home";
+      window.location.href = "https://nomad.wono.co";
     }
   };
 
