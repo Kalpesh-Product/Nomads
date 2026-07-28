97df55df317b2d7eca62b1476326dab9fe2e56ce
pranav-wono <pranavv.wono@gmail.com>
2026-07-28 16:12:56 +0530

    axios fixed for aws

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,27 +1,17 @@
 // src/utils/axios.js
 import axios from "axios";
 
-// const axiosPrivate = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL,
-//   withCredentials: true,
-// });
+const baseURL = import.meta.env.MODE === "production"
+  ? import.meta.env.VITE_PROD_LINK
+  : import.meta.env.VITE_DEV_LINK;
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_DEV_LINK,
+  baseURL,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: import.meta.env.VITE_DEV_LINK,
+  baseURL,
   withCredentials: true,
 });
-
-// export const api = axios.create({
-//   baseURL: import.meta.env.VITE_PROD_LINK,
-// });
-
-// export const axiosPrivate = axios.create({
-//   baseURL: import.meta.env.VITE_PROD_LINK,
-//   withCredentials: true,
-// });
 
 export default axiosPrivate;
