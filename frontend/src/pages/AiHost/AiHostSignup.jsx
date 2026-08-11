c5311644f5ff5b8b9964fedc22750a8c4bccb7c3
allanwono <allan.wono@gmail.com>
2026-08-11 13:14:02 +0530

    email validation text alignment & styling changes

--- frontend/src/pages/AiHost/AiHostSignup.jsx
+++ frontend/src/pages/AiHost/AiHostSignup.jsx
@@ -93,7 +93,16 @@ const floatingLabelSx = {
 };
 
 const compactStepFieldSx = {
+  position: "relative",
   my: 0,
+  "& .MuiFormHelperText-root": {
+    position: "absolute",
+    top: "100%",
+    left: 0,
+    mt: "3px",
+    mx: 0,
+    lineHeight: 1.25,
+  },
 };
 
 const checkedMenuItemSx = {
